e0bb85ce7b4db8e09f6dcd6e0acfac347724974e
BitcoinNero <40897884+BitcoinNero@users.noreply.github.com>
2018-07-21 02:33:49 +0200

    Initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// FOR FORKNOTE : A23AA12A-A11A-A02A-A32A-A20AA02AA00A
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xA2, 0x3A, 0xA1, 0x2A, 0xA1, 0x1A, 0xA0, 0x2A, 0xA3, 0x2A, 0xA2, 0x0A, 0xA0, 0x2A, 0xA0, 0x0A } };
+}
+
